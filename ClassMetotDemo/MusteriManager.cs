2369218d942125ecec0915c024eb77c3a37fcb1b
melikeatac4 <melikeatac4@gmail.com>
2021-02-20 13:10:54 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Musteri Eklendi..  "+musteri.Name+" "+musteri.Surname);
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Musteri Silindi..  " + musteri.Name + " " + musteri.Surname);
+        }
+        public void Listele(Musteri[]musteriler)
+        {
+            Console.WriteLine("Musteri Listesi");
+            foreach(var kisi in musteriler)
+            {
+                Console.WriteLine(kisi.Name+" "+kisi.Surname);
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Name = "Melike";
+            musteri1.Surname = "Ataç";
+            Musteri musteri2 = new Musteri();
+            musteri2.Name = "Beyza";
+            musteri2.Surname = "Yanar";
+            Musteri[] musteriListesi = new Musteri[] { musteri1, musteri2 };
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Sil(musteri2);
+            musteriManager.Listele(musteriListesi);
+
+        
+        }
+
+    }
+}
