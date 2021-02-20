e8325e1213de878a6c15094d522f85650487b000
melikeatac4 <melikeatac4@gmail.com>
2021-02-20 13:21:37 +0300

    Class Metot Demo Project

--- ClassMetotDemo/Musteri.cs
+++ ClassMetotDemo/Musteri.cs
@@ -9,7 +9,6 @@ namespace ClassMetotDemo
         public int Id { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
-        Musteri[] musteriler = new Musteri[] { };
 
 
     }
